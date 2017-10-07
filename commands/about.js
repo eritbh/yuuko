a2582aaa7a3366d04005cda1be71413f94114903
George <georgej1088@gmail.com>
2017-10-07 15:43:02 -0400

    Replace uptime command with about command
    Gets uptime information as well as URLs and version information automatically

---
+++ commands/about.js
@@ -0,0 +1,35 @@
+const Command = require('../src/Command')
+const moment = require('moment-timezone')
+const childProcess = require('child_process')
+const packageJson = require.main.require('./package.json')
+
+module.exports = new Command(['about', 'uptime', 'info'], (c, msg, args, prefix) => {
+    const uptimeDuration = moment.duration(c.uptime).humanize()
+    const uptimeStart = moment().subtract(c.uptime).tz('America/New_York').format('YYYY-DD-mm kk:mm z')
+    const link = packageJson.homepage;
+    childProcess.exec('git describe --abbrev=0 --tags', (err, tag) => {
+        if (err) {
+            tag = '???'
+            console.log(err)
+        } else {
+            tag = tag.replace('\n', '')
+        }
+        childProcess.exec('git rev-parse --short HEAD', (err, sha) => {
+            if (err) {
+                sha = 'Unknown'
+                console.log(err)
+            } else {
+                sha = '`' + sha + '`'
+            }
+            msg.channel.createMessage(`**=== About Yuuko ===**
+**Project:** ${link}
+**Version:** ${tag} (Commit: ${sha})
+**Uptime:** ${uptimeDuration} (since ${uptimeStart})
+Please report any bugs or feedback by creating an issue on the repository.
+Use \`${prefix}help\` to get help using the bot!`)
+        })
+    })
+}, {
+    desc: 'Displays information about the bot, including running version and ',
+    args: ''
+})
