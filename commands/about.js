65f7dbc50b82e47834436eb3b8fbe02f8a0474fb
George <georgej1088@gmail.com>
2017-10-07 16:33:00 -0400

    Fix incomplete description of about command

--- commands/about.js
+++ commands/about.js
@@ -12,24 +12,24 @@ module.exports = new Command(['about', 'uptime', 'info'], (c, msg, args, prefix)
             tag = '???'
             console.log(err)
         } else {
-            tag = tag.replace('\n', '')
+            tag = '`' + tag.replace('\n', '') + '`'
         }
         childProcess.exec('git rev-parse --short HEAD', (err, sha) => {
             if (err) {
                 sha = 'Unknown'
                 console.log(err)
             } else {
-                sha = '`' + sha + '`'
+                sha = '`' + sha.replace('\n', '') + '`'
             }
             msg.channel.createMessage(`**=== About Yuuko ===**
+*(Use \`${prefix}help\` to get help using the bot!)*
 **Project:** ${link}
 **Version:** ${tag} (Commit: ${sha})
 **Uptime:** ${uptimeDuration} (since ${uptimeStart})
-Please report any bugs or feedback by creating an issue on the repository.
-Use \`${prefix}help\` to get help using the bot!`)
+Please report any bugs or feedback by creating an issue on the repository.`)
         })
     })
 }, {
-    desc: 'Displays information about the bot, including running version and ',
+    desc: 'Displays information about the bot, including running version and time since last crash.',
     args: ''
 })
