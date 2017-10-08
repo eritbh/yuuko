3b71b956a15cc85a0ca15aa295bf0fda274539b8
George <georgej1088@gmail.com>
2017-10-08 17:39:57 -0400

    Create setstatus command
    Sets the bot's current status and game text

---
+++ commands/setstatus.js
@@ -0,0 +1,35 @@
+const Command = require('../src/Command')
+
+module.exports = new Command(['setstatus', 'setgame'], (c, msg, args) => {
+  c.getOAuthApplication().then(app => {
+    if (app.owner.id !== msg.author.id) {
+      return msg.channel.createMessage("You're not my dad.")
+    }
+    let status = args.splice(0, 1)[0]
+    switch (status) {
+      case 'dnd':
+      case 'red':
+        status = 'dnd'
+        break
+      case 'idle':
+      case 'yellow':
+        status = 'idle'
+        break
+      case 'invisible':
+      case 'invis':
+      case 'grey':
+      case 'none':
+        status = 'invisible'
+        break
+      case 'online':
+      case 'green':
+        status = 'online'
+        break
+      default:
+        args.splice(0, 0, status)
+        status = 'online'
+    }
+    const game = args.join(' ')
+    c.editStatus(status, game ? {name: game} : undefined)
+  })
+})
