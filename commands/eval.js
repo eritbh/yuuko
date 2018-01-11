20643bce657ba8b56ae35aae6f963f859518d18b
George Platko <georgej1088@gmail.com>
2018-01-11 14:26:53 +0000

    Messy promise resolution in eval command

--- commands/eval.js
+++ commands/eval.js
@@ -7,6 +7,7 @@ module.exports = new Command('eval', function (msg, args, prefix, commandName) {
       return msg.channel.createMessage("You're not my dad.")
     }
 
+    // Parse out code blocks
     args = args.join(' ').replace(/^\s+/, '').replace(/\s*$/, '')
     if (args.startsWith('```') && args.endsWith('```')) {
       args = args.substring(3, args.length - 3)
@@ -15,25 +16,47 @@ module.exports = new Command('eval', function (msg, args, prefix, commandName) {
       }
     }
 
-    // Do some weird shit here because why not
-    let og = console.log
-    let response
-    let ___console = '' // trying really hard to make this not noticeable
-    args = `console.log = function (...args) {
-      ___console += args.join(' ') + '\\n'
-    };` + args
-    try {
-      response = eval(args) // eslint-disable-line no-eval
-    } catch (e) {
-      response = e
+    // Create a dummy console object
+    const c = {
+      _lines: [],
+      _logger (...things) {
+        this._lines.push(...things.join(' ').split('\n'))
+      },
+      _formatLines () {
+        return this._lines.map(line => line && `//> ${line}\n`).join('')
+      }
     }
-    console.log = og
-    ___console = ___console.split(/\n/g).map(line => line ? '//> ' + line : '').join('\n').replace(/\n+$/, '')
-    const result = '```js\n' + ___console + ((___console && response === undefined) ? '' : util.inspect(response)) + '\n```'
+    c.log = c.error= c.warn = c.info = c._logger
 
-    msg.channel.createMessage(result).catch(err => {
+    // Do some weird shit here because why not
+    let result
+    try {
+      result = eval(args) // eslint-ignore-line no-eval
+    } catch (e) {
+      result = e
+    }
+    const message = '```js\n' + c._formatLines() + util.inspect(result) + '\n```'
+
+    const messagePromise = msg.channel.createMessage(message).catch(err => {
       msg.channel.createMessage('Error sending message:\n```\n' + err + '\n```')
     })
+
+    // We returned a promise?
+    if (result && typeof result.then === 'function') {
+      console.log('promise')
+      // Sweet. Wait for that to resolve and the message to send.
+      Promise.all([messagePromise, result]).then(([outputMsg, value]) => {
+        console.log('maybe?')
+        console.log(outputMsg, value)
+        // Now we can edit the message with the promise's resolved result(s).
+        value = util.inspect(value, {depth: 1})
+        const newContent = outputMsg.content.split('\n')
+        newContent.splice(newContent.length - 1, 0, `// Resolved to:`, value)
+        outputMsg.edit(newContent.join('\n'))
+      }, (err) => {
+        console.log(err)
+      })
+    }
   }).catch(e => {
     console.log(e)
   })
