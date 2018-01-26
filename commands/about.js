a9a68af49fe718a3e49a9121a583830b7599db44
George <georgej1088@gmail.com>
2018-01-25 23:18:35 -0500

    Don't run sys commands for every about command, and track ping

--- commands/about.js
+++ commands/about.js
@@ -5,31 +5,42 @@ const moment = require('moment-timezone')
 const childProcess = require('child_process')
 const packageJson = require.main.require('./package.json')
 
+let versionTag
+childProcess.exec('git describe --abbrev=0 --tags', (err, tag) => {
+	if (err) {
+		versionTag = '???'
+		console.log(err)
+	} else {
+		versionTag = '``' + tag.replace('\n', '') + '``'
+	}
+})
+let versionSha
+childProcess.exec('git rev-parse --short HEAD', (err, sha) => {
+	if (err) {
+		versionSha = 'Unknown'
+		console.log(err)
+	} else {
+		versionSha = '`' + sha.replace('\n', '') + '`'
+	}
+})
+
 module.exports = new Command(['about', 'uptime', 'info'], function (msg, args, prefix) {
 	const uptimeDuration = moment.duration(this.uptime).humanize()
 	const uptimeStart = moment().subtract(this.uptime).tz('America/New_York').format('YYYY-DD-mm kk:mm z')
 	const link = packageJson.homepage
-	childProcess.exec('git describe --abbrev=0 --tags', (err, tag) => {
-		if (err) {
-			tag = '???'
-			console.log(err)
-		} else {
-			tag = '`' + tag.replace('\n', '') + '`'
-		}
-		childProcess.exec('git rev-parse --short HEAD', (err, sha) => {
-			if (err) {
-				sha = 'Unknown'
-				console.log(err)
-			} else {
-				sha = '`' + sha.replace('\n', '') + '`'
-			}
-			msg.channel.createMessage(`**=== About Yuuko ===**
+
+	const content = `**=== About Yuuko ===**
 *(Use \`${prefix}help\` to get help using the bot!)*
 **Project:** ${link}
-**Version:** ${tag} (Commit: ${sha})
+**Version:** ${versionTag} (Commit: ${versionSha})
 **Uptime:** ${uptimeDuration} (since ${uptimeStart})
-Please report any bugs or feedback by creating an issue on the repository.`)
-		})
+**Ping:** Wait for it...
+Please report any bugs or feedback by creating an issue on the repository.`
+
+	const then = Date.now()
+	msg.channel.createMessage(content).then(newmsg => {
+		const diff = Date.now() - then
+		newmsg.edit(newmsg.content.replace('Wait for it...', `${diff}ms`))
 	})
 })
 module.exports.help = {
