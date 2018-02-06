7ce1747f007f8f7253dae64881f7e2c59f2ce4ba
George Platko <georgej1088@gmail.com>
2018-02-06 13:59:19 +0000

    eslint --fix

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -9,27 +9,27 @@ module.exports = new Command(['setstatus', 'setgame'], function (msg, args) {
 		}
 		let status = args.splice(0, 1)[0]
 		switch (status) {
-		case 'dnd':
-		case 'red':
-			status = 'dnd'
-			break
-		case 'idle':
-		case 'yellow':
-			status = 'idle'
-			break
-		case 'invisible':
-		case 'invis':
-		case 'grey':
-		case 'none':
-			status = 'invisible'
-			break
-		case 'online':
-		case 'green':
-			status = 'online'
-			break
-		default:
-			args.splice(0, 0, status)
-			status = 'online'
+			case 'dnd':
+			case 'red':
+				status = 'dnd'
+				break
+			case 'idle':
+			case 'yellow':
+				status = 'idle'
+				break
+			case 'invisible':
+			case 'invis':
+			case 'grey':
+			case 'none':
+				status = 'invisible'
+				break
+			case 'online':
+			case 'green':
+				status = 'online'
+				break
+			default:
+				args.splice(0, 0, status)
+				status = 'online'
 		}
 		const game = args.join(' ')
 		this.editStatus(status, game ? {name: game} : undefined)
