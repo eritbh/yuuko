69a3102c77b4f55e9f28a0dcf14886ff324d208b
George Platko <georgej1088@gmail.com>
2018-09-22 20:02:27 +0000

    Add owner-only guilds command
    Gets a list of the top 10 largest guilds the bot is in

---
+++ commands/guilds.js
@@ -0,0 +1,22 @@
+const Command = require('../src/Command');
+
+module.exports = new Command('usage', function (msg) {
+	const guilds = `\`\`\`\n${
+		Array.from(this.guilds.values())
+			// Descending sort
+			.sort((a, b) => b.members.size - a.members.size)
+			// Take top 10 entries
+			.slice(0, 10)
+			// Generate line for each guild
+			.map((g, i) => {
+				const number = `${i + 1}.`.padStart(4);
+				const users = `${g.members.size}`.padStart(6);
+				return `${number} ${users}  ${g.name}`
+			})
+			// Join lines
+			.join('\n')
+	}\n\`\`\``;
+	msg.channel.createMessage(`**=== Largest Guilds ===**\n${guilds}`);
+}, {
+	owner: true
+});
