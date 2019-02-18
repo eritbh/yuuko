e46e528efdfe7b72dbea93231d8d5fd609a11001
George <georgej1088@gmail.com>
2019-02-18 12:58:00 -0500

    also add args to preCommand because why not

--- src/Client.js
+++ src/Client.js
@@ -107,7 +107,7 @@ class Client extends Eris.Client {
 		const command = this.commandForName(commandName);
 		if (!command) return;
 
-		this.emit('preCommand', command, msg);
+		this.emit('preCommand', command, msg, args);
 		await command.execute(this, msg, args, prefix, commandName);
 		this.emit('command', command, msg, args);
 	}
