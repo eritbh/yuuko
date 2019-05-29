4c4127391b4d662ac72497d55e92f46010a3779d
George <georgej1088@gmail.com>
2019-05-29 19:38:37 -0400

    Export Eris from the module for convenience

--- src/Yuuko.ts
+++ src/Yuuko.ts
@@ -3,3 +3,7 @@
 // Exort all things from other files
 export * from './Client';
 export * from './Command';
+
+// Also export plain Eris for convenience working with its types/etc
+import * as Eris from 'eris';
+export {Eris};
