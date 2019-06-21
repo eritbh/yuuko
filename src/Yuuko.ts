40eba7d69fbdfad32ccba8c26996de999a33bf24
George <georgej1088@gmail.com>
2019-06-21 15:59:32 -0400

    Don't export the example commands

--- src/Yuuko.ts
+++ src/Yuuko.ts
@@ -7,19 +7,3 @@ export * from './Command';
 // Also export plain Eris for convenience working with its types/etc
 import * as Eris from 'eris';
 export {Eris};
-
-// Export a namespace for default commands as well
-import debugCommand from './commands/debug';
-import helpCommand from './commands/help';
-import reloadCommand from './commands/reload';
-import setavatarCommand from './commands/setavatar';
-import setnameCommand from './commands/setname';
-import setstatusCommand from './commands/setstatus';
-export namespace defaultCommands {
-	export const debug = debugCommand;
-	export const help = helpCommand;
-	export const reload = reloadCommand;
-	export const setavatar = setavatarCommand;
-	export const setname = setnameCommand;
-	export const setstatus = setstatusCommand;
-}
