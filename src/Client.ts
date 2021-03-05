0d39277a1af3b60e5547c1aa85d267d4ebede04a
Erin <erin20913@gmail.com>
2021-03-05 16:27:38 -0500

    make commandForName handle the default command

--- src/Client.ts
+++ src/Client.ts
@@ -393,9 +393,11 @@ export class Client extends Eris.Client implements ClientOptions {
 
 	/**
 	 * Checks the list of registered commands and returns one whch is known by a
-	 * given name.
+	 * given name. Passing an empty string will return the default command, if
+	 * any.
 	 */
 	commandForName (name: string): Command | null {
+		if (name === '') return this.defaultCommand;
 		if (this.caseSensitiveCommands) return this.commands.find(c => c.names.includes(name)) || null;
 		return this.commands.find(c => c.names.some(n => n.toLowerCase() === name.toLowerCase())) || null;
 	}
