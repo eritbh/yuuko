cc86ff70be9937c9438b342c9b73896c0eb88e43
Erin <erin20913@gmail.com>
2021-04-30 00:52:05 -0400

    Clean up export file, add MemberArgument

--- src/Yuuko.ts
+++ src/Yuuko.ts
@@ -1,13 +1,21 @@
 /** @module Yuuko */
 
 // Export all things from other files
+
+// Main classes
 export * from './Client';
 export * from './Command';
 export * from './EventListener';
 
+// Argument parsing core
 export * from './argumentParsing';
+
+// Argument types for basic values
 export * from './argumentTypes/basic/IntegerArgument';
 export * from './argumentTypes/basic/NumberArgument';
+
+// Argument types for Discord stuff
+export * from './argumentTypes/discord/MemberArgument';
 
 // Also export plain Eris for convenience working with its types/etc
 import * as Eris from 'eris';
