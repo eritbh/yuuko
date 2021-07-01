163e86882244cf737b7f951bec3b0cc85bffd6c6
Erin <erin20913@gmail.com>
2021-07-01 10:53:06 -0400

    Better eval (#54)
    * Expose more convenience variables in eval
    
    * handle ```javascript ``` blocks

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -16,6 +16,9 @@ export default new Command('debug', async function debug (msg, args, ctx) {
 		if (string.startsWith('js')) {
 			string = string.substr(2);
 		}
+		if (string.startsWith('javascript')) {
+			string = string.substr(10);
+		}
 	}
 
 	// Create a dummy console object
@@ -30,8 +33,14 @@ export default new Command('debug', async function debug (msg, args, ctx) {
 	};
 	// eslint-disable-next-line no-multi-assign
 	console.log = console.error = console.warn = console.info = console._logger.bind(console);
-	// eslint-disable-next-line
-	const c = console; // Convenience
+
+	// Convenience variables exposed to eval
+	/* eslint-disable */
+	const c = console;
+	const message = msg;
+	const context = ctx;
+	const {client, prefix, commandName} = ctx;
+	/* eslint-enable */
 
 	// Eval the things and send the results
 	let result;
@@ -40,12 +49,12 @@ export default new Command('debug', async function debug (msg, args, ctx) {
 	} catch (e) {
 		result = e;
 	}
-	const message = `\`\`\`js\n${console._formatLines()}${util.inspect(result, inspectOptions)}\n\`\`\``;
+	const outputText = `\`\`\`js\n${console._formatLines()}${util.inspect(result, inspectOptions)}\n\`\`\``;
 
 	// Send the message
 	let outputMsg;
 	try {
-		outputMsg = await msg.channel.createMessage(message);
+		outputMsg = await msg.channel.createMessage(outputText);
 	} catch (err) {
 		try {
 			msg.channel.createMessage(`Error sending message:\n\`\`\`\n${err}\n\`\`\``);
