94de97418aeaedaee1ead06bc41b48a7fc77e25f
Erin <erin20913@gmail.com>
2021-09-01 19:40:09 -0400

    reload command: reloadCommands -> reloadFiles

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -5,7 +5,7 @@ import {Command} from '../Yuuko';
 export default new Command('reload', (msg, args, {client}) => {
 	msg.channel.sendTyping();
 	setTimeout(() => { // Delay by 100ms to make sure the sendTyping arrives first
-		client.reloadCommands();
+		client.reloadFiles();
 		msg.channel.createMessage('Reloaded commands.');
 	}, 100);
 }, {
